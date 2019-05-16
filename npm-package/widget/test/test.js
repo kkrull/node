c7d39b2ab4e09a8d4d853cd78eafd191911b4480
Kyle Krull <kkrull@8thlight.com>
2019-05-16 11:13:41 -0500

    Update test syntax

--- npm-package/widget/test/test.js
+++ npm-package/widget/test/test.js
@@ -1,8 +1,8 @@
-var assert = require('assert');
+const assert = require('assert');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('Array', () => {
+  describe('#indexOf', () => {
+    it('returns -1, when the value is not present', () => {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
