01919a86e0daf37019b2ea20edb879365184511c
Kyle Krull <kkrull@8thlight.com>
2019-05-16 17:45:16 -0500

    Introduce Commander

--- npm-package/herald/main.js
+++ npm-package/herald/main.js
@@ -1,6 +1,14 @@
 const Greeter = require('greeter').Greeter;
 const NameGenerator = require('generator').NameGenerator;
 
+const Commander = function() { };
+Commander.prototype.assignMission = () => {
+  return 'solve one problem without creating any new ones';
+};
+
+const commander = new Commander();
 const generator = new NameGenerator();
 const greeter = new Greeter();
+
 console.log(greeter.greet(generator.nextName()));
+console.log('Run along now, and ' + commander.assignMission());
