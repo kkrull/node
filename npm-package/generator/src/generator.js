c3b079bfe10a3c06c904c152d0881975e122569c
Kyle Krull <kkrull@8thlight.com>
2019-05-16 13:12:03 -0500

    Remove debug output

--- npm-package/generator/src/generator.js
+++ npm-package/generator/src/generator.js
@@ -3,15 +3,12 @@ const namor = require('namor');
 const NameGenerator = function() { };
 
 NameGenerator.prototype.nextName = () => {
-  const name = namor.generate({
+  return namor.generate({
     char: ' ',
     manly: true,
     numbers: 0,
     words: 2
   });
-
-  console.log(`generated: ${name}`);
-  return name;
 };
 
 module.exports.NameGenerator = NameGenerator;
