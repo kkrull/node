83102fb62c204c90d1caa279451afdaebf33cd3e
Kyle Krull <kkrull@8thlight.com>
2019-12-16 12:28:24 -0600

    Make room for other APIs

--- express-proxy/main.js
+++ express-proxy/main.js
@@ -4,10 +4,9 @@ const bindHost = '127.0.0.1';
 const bindPort = 3000;
 
 const app = express();
-app.get('/', function (req, res) {
+app.get('/hello', (req, res) => {
   res.send('Hello World');
-})
- 
+});
 
 app.listen(bindPort, bindHost, () => {
   console.log(`Listening on http://${bindHost}:${bindPort}`);
