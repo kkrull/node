131407187a3d506a3143a826bb963a13a6f271dc
Kyle Krull <kkrull@8thlight.com>
2019-12-16 12:45:08 -0600

    Log requests

--- express-proxy/main.js
+++ express-proxy/main.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const proxy = require('express-http-proxy');
+const morgan = require('morgan');
 
 const bindHost = '127.0.0.1';
 const bindPort = 3000;
@@ -7,6 +9,9 @@ const app = express();
 app.get('/hello', (req, res) => {
   res.send('Hello World');
 });
+
+app.use(morgan('combined'));
+app.use('/api', proxy('http://dev.ghbeta.com:8950'));
 
 app.listen(bindPort, bindHost, () => {
   console.log(`Listening on http://${bindHost}:${bindPort}`);
