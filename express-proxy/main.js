bef8fc7c0c3e9a6050c2850e14d4ed1c60a9cf30
Kyle Krull <kkrull@8thlight.com>
2019-12-16 12:24:22 -0600

    Start a node.js server

---
+++ express-proxy/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const bindHost = '127.0.0.1';
+const bindPort = 3000;
+
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+ 
+
+app.listen(bindPort, bindHost, () => {
+  console.log(`Listening on http://${bindHost}:${bindPort}`);
+});
---
+++ express-proxy/readme.md
@@ -0,0 +1,6 @@
+## Express Proxy Server
+
+Proxy requests, so everything is coming from the same origin.
+
+Run with `npm start`.
+
