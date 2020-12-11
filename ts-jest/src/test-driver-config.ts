6237bd22c3bdb7e12e32f31f08c60b92125fec7b
Kyle Krull <kkrull@8thlight.com>
2020-12-11 09:29:55 -0600

    Simplify

--- ts-jest/src/test-driver-config.ts
+++ ts-jest/src/test-driver-config.ts
@@ -16,11 +16,12 @@ export default class TestDriverConfig {
   pickApi(options: PickApiOptions = {}): string {
     if(options.preferredApi && this.isSupported(options.preferredApi)) {
       return options.preferredApi;
-    } else if(options.preferredApi === 'mock-monitored' && this.isExternallyMonitored && this.isSupported('mock')) {
+    } else if(options.preferredApi === 'mock-monitored' && this.isExternallyMonitored) {
       return 'mock';
     } else if(options.preferredApi === 'mock-monitored' && !this.isExternallyMonitored && this.isSupported('real')) {
       return 'real';
     } else {
+      //Default to mock mode when no preference given (integration) or fall back when preferred API not supported
       return 'mock';
     }
   }
