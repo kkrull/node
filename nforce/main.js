a3fd76ee4feb4cf53d4944046e5d2d5414a8982d
Kyle Krull <kkrull@8thlight.com>
2021-07-15 17:46:51 -0500

    Authorize with SalesForce

--- nforce/main.js
+++ nforce/main.js
@@ -13,10 +13,26 @@ function loadEnvironmentSettings(environment) {
 loadEnvironmentSettings(process.env.NODE_ENV);
 
 const clientId = process.env['CLIENT_ID'];
-console.log(`Connecting to SalesForce with client: ${clientId}`);
+const clientSecret = process.env['CLIENT_SECRET'];
+const redirectUri = 'http://localhost:3000/oauth/_callback';
 
-// const org = nforce.createConnection({
-//   clientId: '',
-//   clientSecret: '',
-//   redurectUri: '',
-// });
+console.log(`Connecting to SalesForce with client: ${clientId}`);
+const org = nforce.createConnection({
+  authEndpoint:
+    'https://8thlight-dev-ed.my.salesforce.com/services/oauth2/authorize',
+  clientId,
+  clientSecret,
+  redirectUri,
+});
+
+const username = process.env['USERNAME'];
+const password = process.env['PASSWORD'];
+const securityToken = process.env['SECURITY_TOKEN'];
+
+console.log(`Authenticating with user: ${username}`);
+org.authenticate(
+  { username, password, securityToken },
+  function (err, response) {
+    console.log('[Connection#authenticate]', err, response);
+  }
+);
