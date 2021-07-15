55d6d05c89575b2ceed123d3a8e055bf1e3c13d3
Kyle Krull <kkrull@8thlight.com>
2021-07-15 17:05:22 -0500

    Install dotenv for enviornment settings

--- nforce/main.js
+++ nforce/main.js
@@ -1,9 +1,20 @@
 const nforce = require('nforce');
+const path = require('path');
+const process = require('process');
 
-console.log('Hello world!');
-
-const org = nforce.createConnection({
-  clientId: '',
-  clientSecret: '',
-  redurectUri: '',
+const envName = path.join(__dirname, `.env.${process.env.NODE_ENV}`);
+console.log(`Loading environment settings: ${envName}`);
+require('dotenv').config({
+  path: path.join(__dirname, `.env.${process.env.NODE_ENV}`),
 });
+
+console.log('Connecting to SalesForce:', {
+  clientId: process.env['CLIENT_ID'],
+  clientSecret: process.env['CLIENT_SECRET'],
+});
+
+// const org = nforce.createConnection({
+//   clientId: '',
+//   clientSecret: '',
+//   redurectUri: '',
+// });
